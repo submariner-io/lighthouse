fa622741b9b5a4fb1ea682e692e679e67d024224
Mike Kolesnik <mkolesni@redhat.com>
2020-03-29 16:36:26 +0300

    Migrate to shipyard
    Updated scripts and build process to use shipyard

--- scripts/kind-e2e/e2e.sh
+++ scripts/kind-e2e/e2e.sh
@@ -1,179 +1,30 @@
 #!/usr/bin/env bash
+
+## Process command line flags ##
+
+source /usr/share/shflags/shflags
+DEFINE_string 'kubefed' 'false' "Deploy with kubefed"
+DEFINE_string 'lhmode' '' "Mode to deploy (plugin or not)"
+DEFINE_string 'logging' 'false' "Deploy with logging"
+DEFINE_string 'status' 'onetime' "Status flag (onetime, create, keep, clean)"
+FLAGS "$@" || exit $?
+eval set -- "${FLAGS_ARGV}"
+
+kubefed="${FLAGS_kubefed}"
+lhmode="${FLAGS_lhmode}"
+logging="${FLAGS_logging}"
+status="${FLAGS_status}"
+echo "Running with: kubefed=${kubefed}, logging=${logging}, status=${status}"
+
 set -em
 
-source $(dirname $0)/../lib/version
-source $(dirname $0)/../lib/debug_functions
+source ${SCRIPTS_DIR}/lib/debug_functions
+source ${SCRIPTS_DIR}/lib/version
+source ${SCRIPTS_DIR}/lib/utils
 
 ### Functions ###
 
-function kind_clusters() {
-    trap_commands
-    status=$1
-    version=$2
-    pids=(-1 -1 -1)
-    logs=()
-    for i in 1 2 3; do
-        if [[ $(kind get clusters | grep cluster${i} | wc -l) -gt 0  ]]; then
-            echo "Cluster cluster${i} already exists, skipping cluster creation..."
-        else
-            logs[$i]=$(mktemp)
-            echo Creating cluster${i}, logging to ${logs[$i]}...
-            (
-            if [[ -n ${version} ]]; then
-                kind create cluster --image=kindest/node:v${version} --name=cluster${i} --wait=5m --config=${PRJ_ROOT}/scripts/kind-e2e/config/cluster${i}-config.yaml
-            else
-                kind create cluster --name=cluster${i} --wait=5m --config=${PRJ_ROOT}/scripts/kind-e2e/config/cluster${i}-config.yaml
-            fi
-            master_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cluster${i}-control-plane | head -n 1)
-            sed -i -- "s/user: kubernetes-admin/user: cluster$i/g" $(kind get kubeconfig-path --name="cluster$i")
-            sed -i -- "s/name: kubernetes-admin.*/name: cluster$i/g" $(kind get kubeconfig-path --name="cluster$i")
-            sed -i -- "s/current-context: kubernetes-admin.*/current-context: cluster$i/g" $(kind get kubeconfig-path --name="cluster$i")
-
-            if [[ ${status} = keep ]]; then
-                cp -r $(kind get kubeconfig-path --name="cluster$i") ${PRJ_ROOT}/output/kind-config/local-dev/kind-config-cluster${i}
-            fi
-
-            sed -i -- "s/server: .*/server: https:\/\/$master_ip:6443/g" $(kind get kubeconfig-path --name="cluster$i")
-            cp -r $(kind get kubeconfig-path --name="cluster$i") ${PRJ_ROOT}/output/kind-config/dapper/kind-config-cluster${i}
-            ) > ${logs[$i]} 2>&1 &
-            set pids[$i] = $!
-        fi
-    done
-    if [[ ${#logs[@]} -gt 0 ]]; then
-        echo "(Watch the installation processes with \"tail -f ${logs[@]}\".)"
-        for i in 1 2 3; do
-            if [[ pids[$i] -gt -1 ]]; then
-                wait ${pids[$i]}
-                if [[ $? -ne 0 && $? -ne 127 ]]; then
-                    echo Cluster $i creation failed:
-                    cat $logs[$i]
-                fi
-                rm -f $logs[$i]
-            fi
-        done
-    fi
-}
-
-function install_helm() {
-    trap_commands
-    helm init --client-only
-    helm repo add submariner-latest https://releases.rancher.com/submariner-charts/latest
-    pids=(-1 -1 -1)
-    logs=()
-    for i in 1 2 3; do
-        if kubectl --context=cluster${i} -n kube-system rollout status deploy/tiller-deploy > /dev/null 2>&1; then
-            echo "Helm already installed on cluster${i}, skipping helm installation..."
-        else
-            logs[$i]=$(mktemp)
-            echo "Installing helm on cluster${i}, logging to ${logs[$i]}..."
-            (
-            kubectl --context=cluster${i} -n kube-system create serviceaccount tiller
-            kubectl --context=cluster${i} create clusterrolebinding tiller --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-            helm --kube-context cluster${i} init --service-account tiller
-            kubectl --context=cluster${i} -n kube-system rollout status deploy/tiller-deploy
-            ) > ${logs[$i]} 2>&1 &
-            set pids[$i] = $!
-        fi
-    done
-    if [[ ${#logs[@]} -gt 0 ]]; then
-        echo "(Watch the installation processes with \"tail -f ${logs[@]}\".)"
-        for i in 1 2 3; do
-            if [[ pids[$i] -gt -1 ]]; then
-                wait ${pids[$i]}
-                if [[ $? -ne 0 && $? -ne 127 ]]; then
-                    echo Cluster $i creation failed:
-                    cat $logs[$i]
-                fi
-                rm -f $logs[$i]
-            fi
-        done
-    fi
-}
-
-function setup_broker() {
-    trap_commands
-    if kubectl --context=cluster1 get crd clusters.submariner.io > /dev/null 2>&1; then
-        echo "Submariner CRDs already exist, skipping broker creation..."
-    else
-        echo "Installing broker on cluster1."
-        helm --kube-context cluster1 install submariner-latest/submariner-k8s-broker --name ${SUBMARINER_BROKER_NS} --namespace ${SUBMARINER_BROKER_NS}
-    fi
-
-    SUBMARINER_BROKER_URL=$(kubectl --context=cluster1 -n default get endpoints kubernetes -o jsonpath="{.subsets[0].addresses[0].ip}:{.subsets[0].ports[?(@.name=='https')].port}")
-    SUBMARINER_BROKER_CA=$(kubectl --context=cluster1 -n ${SUBMARINER_BROKER_NS} get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='${SUBMARINER_BROKER_NS}-client')].data['ca\.crt']}")
-    SUBMARINER_BROKER_TOKEN=$(kubectl --context=cluster1 -n ${SUBMARINER_BROKER_NS} get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='${SUBMARINER_BROKER_NS}-client')].data.token}"|base64 --decode)
-}
-
-function setup_cluster2_gateway() {
-    trap_commands
-    if kubectl --context=cluster2 wait --for=condition=Ready pods -l app=submariner-engine -n submariner --timeout=60s > /dev/null 2>&1; then
-            echo "Submariner already installed, skipping submariner helm installation..."
-        else
-            echo "Installing submariner on cluster2..."
-            worker_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cluster2-worker | head -n 1)
-            kubectl --context=cluster2 label node cluster2-worker "submariner.io/gateway=true" --overwrite
-            helm --kube-context cluster2 install submariner-latest/submariner \
-            --name submariner \
-            --namespace submariner \
-            --set ipsec.psk="${SUBMARINER_PSK}" \
-            --set broker.server="${SUBMARINER_BROKER_URL}" \
-            --set broker.token="${SUBMARINER_BROKER_TOKEN}" \
-            --set broker.namespace="${SUBMARINER_BROKER_NS}" \
-            --set broker.ca="${SUBMARINER_BROKER_CA}" \
-            --set submariner.clusterId="cluster2" \
-            --set submariner.clusterCidr="10.245.0.0/16" \
-            --set submariner.serviceCidr="100.95.0.0/16" \
-            --set submariner.natEnabled="false" \
-            --set routeAgent.image.repository="rancher/submariner-route-agent" \
-            --set routeAgent.image.tag="v0.0.1" \
-            --set engine.image.repository="rancher/submariner" \
-            --set engine.image.tag="v0.0.1"
-            echo "Waiting for submariner pods to be Ready on cluster2..."
-            kubectl --context=cluster2 wait --for=condition=Ready pods -l app=submariner-engine -n submariner --timeout=120s
-            kubectl --context=cluster2 wait --for=condition=Ready pods -l app=submariner-routeagent -n submariner --timeout=120s
-            echo "Deploying netshoot on cluster2 worker: ${worker_ip}"
-            kubectl --context=cluster2 apply -f ${PRJ_ROOT}/scripts/kind-e2e/config/netshoot.yaml
-            echo "Waiting for netshoot pods to be Ready on cluster2."
-            kubectl --context=cluster2 rollout status deploy/netshoot --timeout=120s
-    fi
-}
-
-function setup_cluster3_gateway() {
-    trap_commands
-    if kubectl --context=cluster3 wait --for=condition=Ready pods -l app=submariner-engine -n submariner --timeout=60s > /dev/null 2>&1; then
-            echo "Submariner already installed, skipping submariner helm installation..."
-        else
-            echo "Installing submariner on cluster3..."
-            worker_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cluster3-worker | head -n 1)
-            kubectl --context=cluster3 label node cluster3-worker "submariner.io/gateway=true" --overwrite
-            helm --kube-context cluster3 install submariner-latest/submariner \
-             --name submariner \
-             --namespace submariner \
-             --set ipsec.psk="${SUBMARINER_PSK}" \
-             --set broker.server="${SUBMARINER_BROKER_URL}" \
-             --set broker.token="${SUBMARINER_BROKER_TOKEN}" \
-             --set broker.namespace="${SUBMARINER_BROKER_NS}" \
-             --set broker.ca="${SUBMARINER_BROKER_CA}" \
-             --set submariner.clusterId="cluster3" \
-             --set submariner.clusterCidr="10.246.0.0/16" \
-             --set submariner.serviceCidr="100.96.0.0/16" \
-             --set submariner.natEnabled="false" \
-            --set routeAgent.image.repository="rancher/submariner-route-agent" \
-            --set routeAgent.image.tag="v0.0.1" \
-            --set engine.image.repository="rancher/submariner" \
-            --set engine.image.tag="v0.0.1"
-            echo "Waiting for submariner pods to be Ready on cluster3..."
-            kubectl --context=cluster3 wait --for=condition=Ready pods -l app=submariner-engine -n submariner --timeout=120s
-            kubectl --context=cluster3 wait --for=condition=Ready pods -l app=submariner-routeagent -n submariner --timeout=120s
-            echo "Deploying nginx on cluster3 worker: ${worker_ip}"
-            kubectl --context=cluster3 apply -f ${PRJ_ROOT}/scripts/kind-e2e/config/nginx-demo.yaml
-            echo "Waiting for nginx-demo deployment to be Ready on cluster3."
-            kubectl --context=cluster3 rollout status deploy/nginx-demo --timeout=120s
-    fi
-}
-
 function setup_lighthouse_controller () {
-    trap_commands
     for i in 1 2 3; do
       echo "Installing lighthouse CRD in cluster${i}..."
       kubectl --context=cluster${i} apply -f ${PRJ_ROOT}/package/lighthouse-crd.yaml
@@ -186,7 +37,6 @@ function setup_lighthouse_controller () {
 }
 
 function update_coredns_deployment() {
-    trap_commands
     uuid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
     docker tag lighthouse-coredns:${VERSION} lighthouse-coredns:${VERSION}_${uuid}
     for i in 1 2 3; do
@@ -203,7 +53,6 @@ function update_coredns_deployment() {
 }
 
 function update_coredns_configmap() {
-    trap_commands
     for i in 2 3; do
         echo "Updating coredns configMap in cluster${i}..."
         kubectl --context=cluster${i} -n kube-system replace -f ${PRJ_ROOT}/scripts/kind-e2e/config/coredns-cm.yaml
@@ -213,7 +62,6 @@ function update_coredns_configmap() {
 }
 
 function deploy_lighthouse_dnsserver() {
-   trap_commands
     docker tag lighthouse-dnsserver:${VERSION} lighthouse-dnsserver:local
     for i in 2 3; do
         echo "Updating coredns clusterrole in to cluster${i}..."
@@ -228,33 +76,15 @@ function deploy_lighthouse_dnsserver() {
     done
 }
 
-function test_connection() {
-    trap_commands
-    nginx_svc_ip_cluster3=$(kubectl --context=cluster3 get svc -l app=nginx-demo | awk 'FNR == 2 {print $3}')
-    netshoot_pod=$(kubectl --context=cluster2 get pods -l app=netshoot | awk 'FNR == 2 {print $1}')
-
-    echo "Testing connectivity between clusters - $netshoot_pod cluster2 --> $nginx_svc_ip_cluster3 nginx service cluster3"
-
-    attempt_counter=0
-    max_attempts=5
-    until $(kubectl --context=cluster2 exec -it ${netshoot_pod} -- curl --output /dev/null -m 30 --silent --head --fail ${nginx_svc_ip_cluster3}); do
-        if [[ ${attempt_counter} -eq ${max_attempts} ]];then
-          echo "Max attempts reached, connection test failed!"
-          exit 1
-        fi
-        attempt_counter=$(($attempt_counter+1))
-    done
-    echo "Connection test was successful!"
-}
-
 function test_e2e_service_discovery() {
-    trap_commands
     echo "Updating coredns deployment on cluster2 with cluster3 service nginx service ip"
     if kubectl describe federatednamespace default > /dev/null 2>&1; then
         echo "Namespace default already federated..."
     else
         kubefedctl federate namespace default
     fi
+    nginx_svc_ip_cluster3=$(kubectl --context=cluster3 get svc -l app=nginx-demo | awk 'FNR == 2 {print $3}')
+    netshoot_pod=$(kubectl --context=cluster2 get pods -l app=netshoot | awk 'FNR == 2 {print $1}')
     kubectl --context=cluster2 -n kube-system set env deployment/coredns LIGHTHOUSE_SVCS="nginx-demo=${nginx_svc_ip_cluster3}"
     kubectl --context=cluster2 rollout status -n kube-system deploy/coredns --timeout=60s
     echo "Testing service discovery between clusters - $netshoot_pod cluster2 --> nginx service cluster3"
@@ -271,7 +101,6 @@ function test_e2e_service_discovery() {
 }
 
 function enable_logging() {
-    trap_commands
     if kubectl --context=cluster1 rollout status deploy/kibana > /dev/null 2>&1; then
         echo "Elasticsearch stack already installed, skipping..."
     else
@@ -289,10 +118,10 @@ function enable_logging() {
 }
 
 function enable_kubefed() {
-    trap_commands
     if kubectl --context=cluster1 rollout status deploy/kubefed-controller-manager -n ${KUBEFED_NS} > /dev/null 2>&1; then
         echo "Kubefed already installed, skipping setup..."
     else
+        helm init --client-only
         helm repo add kubefed-charts https://raw.githubusercontent.com/kubernetes-sigs/kubefed/master/charts
         helm --kube-context cluster1 install kubefed-charts/kubefed --version=0.1.0-rc2 --name kubefed --namespace ${KUBEFED_NS} --set controllermanager.replicaCount=1
         for i in 1 2 3; do
@@ -305,75 +134,45 @@ function enable_kubefed() {
 }
 
 function test_with_e2e_tests {
-    trap_commands
-    cd ../test/e2e
-
-    # Setup the KUBECONFIG env
-    export KUBECONFIG=$(echo ${PRJ_ROOT}/output/kind-config/dapper/kind-config-cluster{1..3} | sed 's/ /:/g')
+    cd ${DAPPER_SOURCE}/test/e2e
 
     go test -args -ginkgo.v -ginkgo.randomizeAllSpecs \
          -dp-context cluster1  -dp-context cluster2 -dp-context cluster3  \
-        -report-dir ${DAPPER_SOURCE}/${DAPPER_OUTPUT}/junit 2>&1 | \
-        tee ${DAPPER_SOURCE}/${DAPPER_OUTPUT}/e2e-tests.log
+        -report-dir ${DAPPER_OUTPUT}/junit 2>&1 | \
+        tee ${DAPPER_OUTPUT}/e2e-tests.log
 }
 
 function cleanup {
-    trap_commands
-    for i in 1 2 3; do
-      if [[ $(kind get clusters | grep cluster${i} | wc -l) -gt 0  ]]; then
-        kind delete cluster --name=cluster${i};
-      fi
-    done
-
-    if [[ $(docker ps -qf status=exited | wc -l) -gt 0 ]]; then
-        echo Cleaning containers...
-        docker ps -qf status=exited | xargs docker rm -f
-    fi
-    if [[ $(docker images -qf dangling=true | wc -l) -gt 0 ]]; then
-        echo Cleaning images...
-        docker images -qf dangling=true | xargs docker rmi -f
-    fi
-    if [[ $(docker volume ls -qf dangling=true | wc -l) -gt 0 ]]; then
-        echo Cleaning volumes...
-        docker volume ls -qf dangling=true | xargs docker volume rm -f
-    fi
+    "${SCRIPTS_DIR}"/cleanup.sh
 }
 
 ### Main ###
 
-if [[ $1 = clean ]]; then
-    echo Cleaning
+declare_kubeconfig
+
+if [[ $status = clean ]]; then
     cleanup
     exit 0
-fi
-
-if [[ $1 != keep ]]; then
+elif [[ $status = onetime ]]; then
+    echo Status $status: Will cleanup on EXIT signal
     trap cleanup EXIT
+elif [[ $status != keep && $status != create ]]; then
+    echo Unknown status: $status
+    cleanup
+    exit 1
 fi
 
-echo Starting with status: $1, k8s_version: $2, logging: $3, kubefed: $4, coredns=$5.
 PRJ_ROOT=$(git rev-parse --show-toplevel)
-mkdir -p ${PRJ_ROOT}/output/kind-config/dapper/ ${PRJ_ROOT}/output/kind-config/local-dev/
-SUBMARINER_BROKER_NS=submariner-k8s-broker
-SUBMARINER_PSK=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
 KUBEFED_NS=kube-federation-system
-export KUBECONFIG=$(echo ${PRJ_ROOT}/output/kind-config/dapper/kind-config-cluster{1..3} | sed 's/ /:/g')
 
-kind_clusters "$@"
-if [[ $3 = true ]]; then
+if [[ $logging = true ]]; then
     enable_logging
 fi
-install_helm
-if [[ $4 = true ]]; then
+
+if [[ $kubefed = true ]]; then
     enable_kubefed
-fi
-setup_broker
-setup_cluster2_gateway
-setup_cluster3_gateway
-test_connection
-if [[ $4 = true ]]; then
     setup_lighthouse_controller
-    if [[ $6 = plugin ]]; then
+    if [[ $lhmode = plugin ]]; then
         update_coredns_configmap
         update_coredns_deployment
     else
@@ -383,11 +182,11 @@ if [[ $4 = true ]]; then
     test_with_e2e_tests
 fi
 
-if [[ $1 = keep ]]; then
+if [[ $status = keep || $status = create ]]; then
     echo "your 3 virtual clusters are deployed and working properly."
     echo "clusters can be accessed with:"
     echo ""
-    echo "export KUBECONFIG=\$(echo \$(git rev-parse --show-toplevel)/output/kind-config/local-dev/kind-config-cluster{1..3} | sed 's/ /:/g')"
+    echo "export KUBECONFIG=\$(echo \$(git rev-parse --show-toplevel)/output/kubeconfigs/kind-config-cluster{1..3} | sed 's/ /:/g')"
     echo ""
     echo "$ kubectl config use-context cluster1 # or cluster2, cluster3.."
     echo ""
