90c7d2f500c0119f0f1ced3b676106abbd1de131
Aswin Surayanarayanan <asuryana@redhat.com>
2020-06-18 14:31:48 +0530

    Update the Imported service
    Imported Services is updated as per the recent  upstream spec.
    
    Signed-off-by: Aswin Surayanarayanan <asuryana@redhat.com>

--- pkg/apis/lighthouse.submariner.io/v2alpha1/types.go
+++ pkg/apis/lighthouse.submariner.io/v2alpha1/types.go
@@ -74,48 +74,85 @@ type ServiceExportList struct {
 //+k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +genclient:noStatus
 
-// ServiceImport declares that the specified service is imported from other clusters.
+// ServiceImport describes a service imported from clusters in a supercluster.
 type ServiceImport struct {
 	metav1.TypeMeta `json:",inline"`
 	// +optional
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 	// +optional
 	Spec ServiceImportSpec `json:"spec,omitempty"`
+	// +optional
+	Status ServiceImportStatus `json:"status,omitempty"`
 }
 
-// ServiceImportSpec contains the current status of an imported service and the
-// information necessary to consume it.
+// ServiceImportType designates the type of a ServiceImport
+type ServiceImportType string
+
+const (
+	// SuperclusterIP are only accessible via the Supercluster IP.
+	SuperclusterIP ServiceImportType = "SuperclusterIP"
+	// Headless services allow backend pods to be addressed directly.
+	Headless ServiceImportType = "Headless"
+)
+
+// ServiceImportSpec describes an imported service and the information necessary to consume it.
 type ServiceImportSpec struct {
-	// +patchStrategy=merge
-	// +patchMergeKey=port
-	// +listType=map
-	// +listMapKey=port
-	// +listMapKey=protocol
-	Ports []corev1.ServicePort `json:"ports"`
-	// +optional
-	// +patchStrategy=merge
-	// +patchMergeKey=cluster
-	// +listType=map
-	// +listMapKey=cluster
-	Clusters []ClusterSpec `json:"clusters"`
+	// +listType=atomic
+	Ports []ServicePort `json:"ports"`
 	// +optional
 	IP string `json:"ip,omitempty"`
-}
-
-// ClusterSpec contains service configuration mapped to a specific cluster
-type ClusterSpec struct {
-	Cluster string `json:"cluster"`
 	// +optional
-	// +listType=set
-	TopologyKeys []string `json:"topologyKeys"`
-	// +optional
-	PublishNotReadyAddresses bool `json:"publishNotReadyAddresses"`
+	Type ServiceImportType `json:"type"`
 	// +optional
 	SessionAffinity corev1.ServiceAffinity `json:"sessionAffinity"`
 	// +optional
 	SessionAffinityConfig *corev1.SessionAffinityConfig `json:"sessionAffinityConfig"`
 	// +optional
-	IPs []string `json:"ips,omitempty"`
+	MultiClusterIPs []string `json:"ips,omitempty"`
+}
+
+// ServicePort represents the port on which the service is exposed
+type ServicePort struct {
+	// The name of this port within the service. This must be a DNS_LABEL.
+	// All ports within a ServiceSpec must have unique names. When considering
+	// the endpoints for a Service, this must match the 'name' field in the
+	// EndpointPort.
+	// Optional if only one ServicePort is defined on this service.
+	// +optional
+	Name string `json:"name,omitempty"`
+
+	// The IP protocol for this port. Supports "TCP", "UDP", and "SCTP".
+	// Default is TCP.
+	// +optional
+	Protocol corev1.Protocol `json:"protocol,omitempty"`
+
+	// The application protocol for this port.
+	// This field follows standard Kubernetes label syntax.
+	// Un-prefixed names are reserved for IANA standard service names (as per
+	// RFC-6335 and http://www.iana.org/assignments/service-names).
+	// Non-standard protocols should use prefixed names such as
+	// mycompany.com/my-custom-protocol.
+	// Field can be enabled with ServiceAppProtocol feature gate.
+	// +optional
+	AppProtocol *string `json:"appProtocol,omitempty"`
+
+	// The port that will be exposed by this service.
+	Port int32 `json:"port"`
+}
+
+// ServiceImportStatus describes derived state of an imported service.
+type ServiceImportStatus struct {
+	// +optional
+	// +patchStrategy=merge
+	// +patchMergeKey=cluster
+	// +listType=map
+	// +listMapKey=cluster
+	Clusters []ClusterStatus `json:"clusters"`
+}
+
+// ClusterStatus contains service configuration mapped to a specific source cluster
+type ClusterStatus struct {
+	Cluster string `json:"cluster"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
