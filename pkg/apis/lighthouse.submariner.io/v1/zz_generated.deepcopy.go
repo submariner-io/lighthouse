1932ed6b1850d67e7e58901deec7a326e44656aa
Stephen Kitt <skitt@redhat.com>
2020-07-29 12:34:30 +0200

    Bump to Kubernetes 1.17.0
    ... to match Shipyard.
    
    This involves upgrading to CoreDNS 1.6.7 (which supports 1.17) and
    dropping the federation plugin which is no longer available.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- pkg/apis/lighthouse.submariner.io/v1/zz_generated.deepcopy.go
+++ pkg/apis/lighthouse.submariner.io/v1/zz_generated.deepcopy.go
@@ -71,7 +71,7 @@ func (in *MultiClusterService) DeepCopyObject() runtime.Object {
 func (in *MultiClusterServiceList) DeepCopyInto(out *MultiClusterServiceList) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
-	out.ListMeta = in.ListMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
 		*out = make([]MultiClusterService, len(*in))
