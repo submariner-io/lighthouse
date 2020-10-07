67c400f97881dc1de57bc2bab5dc1ab2b2dcf104
Aswin Surayanarayanan <asuryana@redhat.com>
2020-10-07 14:30:15 +0530

    Apply review comments
    Signed-off-by: Aswin Surayanarayanan <asuryana@redhat.com>

--- pkg/serviceimport/map.go
+++ pkg/serviceimport/map.go
@@ -16,7 +16,7 @@ type clusterInfo struct {
 
 type serviceInfo struct {
 	key           string
-	clusterIPs    map[string][]string
+	clusterIPs    map[string]string
 	clustersQueue []clusterInfo
 	rrCount       uint64
 	isHeadless    bool
@@ -26,7 +26,7 @@ func (si *serviceInfo) buildClusterInfoQueue() {
 	si.clustersQueue = make([]clusterInfo, 0)
 	for k, v := range si.clusterIPs {
 		if len(v) > 0 {
-			c := clusterInfo{name: k, ip: v[0], weight: 0}
+			c := clusterInfo{name: k, ip: v, weight: 0}
 			si.clustersQueue = append(si.clustersQueue, c)
 		}
 	}
@@ -54,8 +54,8 @@ func (m *Map) selectIP(queue []clusterInfo, counter *uint64, checkCluster func(s
 	return ""
 }
 
-func (m *Map) GetIPs(namespace, name, cluster string, checkCluster func(string) bool) ([]string, bool) {
-	clusterIPs, queue, counter, isHeadless := func() (map[string][]string, []clusterInfo, *uint64, bool) {
+func (m *Map) GetIP(namespace, name, cluster string, checkCluster func(string) bool) (string, bool) {
+	clusterIPs, queue, counter, isHeadless := func() (map[string]string, []clusterInfo, *uint64, bool) {
 		m.RLock()
 		defer m.RUnlock()
 
@@ -68,20 +68,20 @@ func (m *Map) GetIPs(namespace, name, cluster string, checkCluster func(string) 
 	}()
 
 	if clusterIPs == nil || isHeadless {
-		return nil, false
+		return "", false
 	}
 
 	if cluster != "" {
-		ips, found := clusterIPs[cluster]
-		return ips, found
+		ip, found := clusterIPs[cluster]
+		return ip, found
 	}
 
 	ip := m.selectIP(queue, counter, checkCluster)
 	if ip != "" {
-		return []string{ip}, true
+		return ip, true
 	}
 
-	return []string{}, true
+	return "", true
 }
 
 func NewMap() *Map {
@@ -103,13 +103,13 @@ func (m *Map) Put(serviceImport *lighthousev2a1.ServiceImport) {
 		if !ok {
 			remoteService = &serviceInfo{
 				key:        key,
-				clusterIPs: make(map[string][]string),
+				clusterIPs: make(map[string]string),
 				rrCount:    0,
 				isHeadless: serviceImport.Spec.Type == lighthousev2a1.Headless,
 			}
 		}
 
-		remoteService.clusterIPs[serviceImport.GetLabels()[lhconstants.LabelSourceCluster]] = []string{serviceImport.Spec.IP}
+		remoteService.clusterIPs[serviceImport.GetLabels()[lhconstants.LabelSourceCluster]] = serviceImport.Spec.IP
 
 		if !remoteService.isHeadless {
 			remoteService.buildClusterInfoQueue()
