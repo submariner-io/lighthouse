9a8d60deccd67875fec9b4c12d4393b951088ca3
Tom Pantelis <tompantelis@gmail.com>
2021-01-05 12:00:42 -0500

    Remove fetching of cluster IDs in E2E
    This is now in shipyard.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- test/e2e/framework/framework.go
+++ test/e2e/framework/framework.go
@@ -78,21 +78,6 @@ func beforeSuite() {
 		MCSClients = append(MCSClients, createLighthouseClient(restConfig))
 	}
 
-	framework.By("Fetching ClusterIDs")
-
-	for idx, kubeClient := range framework.KubeClients {
-		agentDeployment, err :=
-			kubeClient.AppsV1().Deployments(framework.TestContext.SubmarinerNamespace).Get("submariner-lighthouse-agent", metav1.GetOptions{})
-		Expect(err).To(Succeed())
-
-		for _, envVar := range agentDeployment.Spec.Template.Spec.Containers[0].Env {
-			if envVar.Name == "SUBMARINER_CLUSTERID" {
-				framework.TestContext.ClusterIDs[idx] = envVar.Value
-				break
-			}
-		}
-	}
-
 	framework.DetectGlobalnet()
 }
 
