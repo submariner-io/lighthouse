dda06bf8fbc7b84213079c782a24af3c1eef4396
Vishal Thapar <5137689+vthapar@users.noreply.github.com>
2021-01-28 23:38:35 +0530

    Use env var for ClusterId in lighthouse-coredns
    Fixes #441
    
    Signed-off-by: Vishal Thapar <5137689+vthapar@users.noreply.github.com>
    (cherry picked from commit 6520558f812ab9b200c1cab16905d29762150420)

--- pkg/gateway/controller.go
+++ pkg/gateway/controller.go
@@ -2,6 +2,7 @@ package gateway
 
 import (
 	"fmt"
+	"os"
 	"sync/atomic"
 
 	"github.com/submariner-io/admiral/pkg/log"
@@ -41,8 +42,13 @@ func NewController() *Controller {
 		stopCh:           make(chan struct{}),
 		gatewayAvailable: true,
 	}
+
 	controller.clusterStatusMap.Store(make(map[string]bool))
-	controller.localClusterID.Store("")
+
+	localClusterId := os.Getenv("SUBMARINER_CLUSTERID")
+
+	klog.Infof("Setting localClusterID from env: %q", localClusterId)
+	controller.localClusterID.Store(localClusterId)
 
 	return controller
 }
