22f78bf708e9f2d18c56a37517718c4d04cafa4d
Daniel Bachar <danibachar89@gmail.com>
2021-12-14 07:14:06 -0700

    Removed mistake with copy annotation from serviceimport to service
    Signed-off-by: Daniel Bachar <danibachar89@gmail.com>

--- pkg/agent/controller/agent.go
+++ pkg/agent/controller/agent.go
@@ -198,9 +198,8 @@ func (a *Controller) Start(stopCh <-chan struct{}) error {
 		return a.serviceImportLister(func(si *mcsv1a1.ServiceImport) runtime.Object {
 			return &corev1.Service{
 				ObjectMeta: metav1.ObjectMeta{
-					Name:        si.GetAnnotations()[lhconstants.OriginName],
-					Namespace:   si.GetAnnotations()[lhconstants.OriginNamespace],
-					Annotations: si.GetAnnotations(),
+					Name:      si.GetAnnotations()[lhconstants.OriginName],
+					Namespace: si.GetAnnotations()[lhconstants.OriginNamespace],
 				},
 			}
 		})
