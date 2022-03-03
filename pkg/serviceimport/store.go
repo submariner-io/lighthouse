69bbb7eb0c03e71082d6e4efe537bf8dff92257c
Daniel Farrell <dfarrell@redhat.com>
2022-03-02 22:41:41 -0500

    Add packagedoc-lint linting, fix errors
    The idea here is to make sure the license headers are detached from the
    package declarations, as otherwise the headers are considered a part of
    the package documentation and included with it.
    
    Add per-PR job to maintain consistent standards across all repos.
    
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>
    (cherry picked from commit 70f817e8638938ebf900fd9d130db4cc235762aa)

--- pkg/serviceimport/store.go
+++ pkg/serviceimport/store.go
@@ -15,6 +15,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package serviceimport
 
 import mcsv1a1 "sigs.k8s.io/mcs-api/pkg/apis/v1alpha1"
