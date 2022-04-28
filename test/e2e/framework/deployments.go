bdaa08eb86e389558983ef1cecaeb0fa67ff788c
Daniel Farrell <dfarrell@redhat.com>
2022-04-28 13:51:05 -0400

    Fix golangci-lint Go linting
    This was introduced in the last commit,
    9a670496922cdc9669334cf5453f892fb95515d3, but should not have made it
    past CI as it is correctly flagged on all relevant versions (1.43.0,
    1.44.2, 1.45.2).
    
    If that commit is backported, this one will need to be as well.
    
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>
    (cherry picked from commit 43b6824b050a213ac351169cb675155b0251f5e3)

--- test/e2e/framework/deployments.go
+++ test/e2e/framework/deployments.go
@@ -22,7 +22,6 @@ import (
 	"context"
 
 	"github.com/submariner-io/shipyard/test/e2e/framework"
-
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
