0c6bbacf26d670c3058320bcd707f0322f6c2214
Automated Release <release@submariner.io>
2023-05-22 14:38:00 -0400

    Include not-ready addresses in EndpointSlice
    Signed-off-by: Automated Release <release@submariner.io>

--- pkg/agent/controller/controller_suite_test.go
+++ pkg/agent/controller/controller_suite_test.go
@@ -62,6 +62,7 @@ const (
 	serviceNamespace = "service-ns"
 	globalIP1        = "242.254.1.1"
 	globalIP2        = "242.254.1.2"
+	globalIP3        = "242.254.1.3"
 )
 
 var (
@@ -293,6 +294,11 @@ func newTestDiver() *testDriver {
 			Conditions: discovery.EndpointConditions{Ready: pointer.Bool(true)},
 			Hostname:   &t.cluster1.endpoints.Subsets[0].Addresses[1].TargetRef.Name,
 			NodeName:   t.cluster1.endpoints.Subsets[0].Addresses[1].NodeName,
+		},
+		{
+			Addresses:  []string{t.cluster1.endpoints.Subsets[0].NotReadyAddresses[0].IP},
+			Conditions: discovery.EndpointConditions{Ready: pointer.Bool(false)},
+			Hostname:   &t.cluster1.endpoints.Subsets[0].NotReadyAddresses[0].TargetRef.Name,
 		},
 	}
 
