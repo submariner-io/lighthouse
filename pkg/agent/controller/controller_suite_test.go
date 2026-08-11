64261fa57e2ea00364b9a2401f988d19a4cbe498
Tom Pantelis <tompantelis@gmail.com>
2026-08-11 15:43:14 -0400

    Add configurable namespace validation for broker imports
    Broker-supplied ServiceImports and EndpointSlices can target arbitrary
    local namespaces via attacker-controlled labels/annotations. This
    enables namespace injection attacks (CWE-441: Confused Deputy) where a
    compromised peer pollutes privileged namespaces (kube-system,
    openshift-*) fleet-wide, bypassing local ServiceExport admission.
    
    Add a NamespaceValidator with configurable deny list via ConfigMap:
    - Default blocks: kube-*, openshift-*, openshift
    - Operators can customize via "import-namespace-deny-list" ConfigMap key
    - Entries with trailing hyphen = prefix match (e.g., "kube-")
    - Entries without hyphen = exact + prefix match (e.g., "openshift")
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/agent/controller/controller_suite_test.go
+++ pkg/agent/controller/controller_suite_test.go
@@ -121,7 +121,6 @@ type cluster struct {
 	agentSpec                  controller.AgentSpecification
 	localDynClient             dynamic.Interface
 	localDynClientFake         *k8stesting.Fake
-	localServiceExportClient   dynamic.ResourceInterface
 	localServiceImportClient   dynamic.NamespaceableResourceInterface
 	localIngressIPClient       dynamic.ResourceInterface
 	localEndpointSliceClient   dynamic.ResourceInterface
@@ -357,9 +356,6 @@ func (c *cluster) init(syncerConfig *broker.SyncerConfig, dynClient dynamic.Inte
 
 	c.localServiceImportReactor = fake.NewFailingReactorForResource(c.localDynClientFake, mcsv1a1.ServiceImportPluralName)
 
-	c.localServiceExportClient = c.localDynClient.Resource(*test.GetGroupVersionResourceFor(syncerConfig.RestMapper,
-		&mcsv1a1.ServiceExport{})).Namespace(serviceNamespace)
-
 	c.localServiceImportClient = c.localDynClient.Resource(*test.GetGroupVersionResourceFor(syncerConfig.RestMapper,
 		&mcsv1a1.ServiceImport{}))
 
@@ -434,11 +430,11 @@ func (c *cluster) deleteService() {
 }
 
 func (c *cluster) createServiceExport() {
-	test.CreateResource(c.localServiceExportClient, c.serviceExport)
+	test.CreateResource(c.localServiceExportClient(), c.serviceExport)
 }
 
 func (c *cluster) deleteServiceExport() {
-	Expect(c.localServiceExportClient.Delete(context.TODO(), c.serviceExport.GetName(), metav1.DeleteOptions{})).To(Succeed())
+	Expect(c.localServiceExportClient().Delete(context.TODO(), c.serviceExport.GetName(), metav1.DeleteOptions{})).To(Succeed())
 }
 
 func (c *cluster) createServiceEndpointSlices() {
@@ -563,7 +559,7 @@ func (c *cluster) awaitServiceExportCondition(expected ...metav1.Condition) {
 	last := len(expected) - 1
 
 	Eventually(func(g Gomega) {
-		obj, err := c.localServiceExportClient.Get(context.Background(), c.serviceExport.Name, metav1.GetOptions{})
+		obj, err := c.localServiceExportClient().Get(context.Background(), c.serviceExport.Name, metav1.GetOptions{})
 		Expect(err).To(Succeed())
 		se := toServiceExport(obj)
 
@@ -571,7 +567,7 @@ func (c *cluster) awaitServiceExportCondition(expected ...metav1.Condition) {
 
 		g.Expect(c).NotTo(BeNil(), "ServiceExport condition not found for type %q", expected[last].Type)
 		assertEquivalentConditions(g, c, &expected[last])
-	}).Should(Succeed())
+	}).Within(time.Second * 3).Should(Succeed())
 }
 
 //nolint:gocritic // Ignore hugeParam
@@ -651,6 +647,10 @@ func (c *cluster) ensureNoServiceExportActions() {
 	Consistently(func() []string {
 		return testutil.GetOccurredActionVerbs(c.localDynClientFake, mcsv1a1.ServiceExportPluralName, "get", "update")
 	}, 500*time.Millisecond).Should(BeEmpty())
+}
+
+func (c *cluster) localServiceExportClient() dynamic.ResourceInterface {
+	return serviceExportClientFor(c.localDynClient, c.serviceExport.Namespace)
 }
 
 func awaitServiceImport(client dynamic.NamespaceableResourceInterface, expected *mcsv1a1.ServiceImport, ipPool *ipam.IPPool,
